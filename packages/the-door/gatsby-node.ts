dd627ac75448045023f9e357f197f87a07e885e8
Clay Giffin <clay@claygiffin.com>
2023-01-09 12:27:44 -0800

    time stuff

--- packages/the-door/gatsby-node.ts
+++ packages/the-door/gatsby-node.ts
@@ -347,7 +347,8 @@ export const createResolvers: GatsbyNode['createResolvers'] = async ({
             type: `SiteBuildMetadata`,
           })
           const today = new Date(buildTime)
-          today.setUTCHours(4, 0, 0, 0)
+          today.getHours() < 5 && today.setUTCDate(today.getDate() - 1)
+          today.setUTCHours(5, 0, 0, 0)
           const { start_date_time, end_date_time } =
             source.entityPayload.attributes
           const cutoff = new Date(end_date_time || start_date_time)
