c4801fa191028087b28dcc5ddfdc8b5ee18d518c
Clay Giffin <clay@claygiffin.com>
2024-03-05 09:49:55 -0700

    door analytics

--- packages/the-door/gatsby-config.ts
+++ packages/the-door/gatsby-config.ts
@@ -131,6 +131,16 @@ const config: GatsbyConfig = {
         },
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "G-86PJKS11B0",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+        enableWebVitalsTracking: true,
+
+      },
+    },
     `gatsby-plugin-netlify`,
   ],
 }
