dce45b8580b991d3af48fdc3c7fd33b2cf219a65
Clay Giffin <clay@claygiffin.com>
2025-04-22 09:49:29 -0700

    og form working

--- packages/common/src/components/OGRegistrationFormRenderer.tsx
+++ packages/common/src/components/OGRegistrationFormRenderer.tsx
@@ -16,20 +16,8 @@ type Props = ComponentProps<'div'> & {
 
 declare global {
   interface Window {
-    BBRegistrationFormLoader: any
+    BBEventRegistrationFormLoader: any
     iFrameResize: any
-    // Blackbaud_GetBlackoutDaysForRecurringDirectDebit: any
-    // Blackbaud_Init: any
-    // Blackbaud_Open: any
-    // Blackbaud_OpenCardNotPresentForm: any
-    // Blackbaud_OpenCardPresentForm: any
-    // Blackbaud_OpenDirectDebitForm: any
-    // Blackbaud_OpenPaymentForm: any
-    // Blackbaud_OpenStoreCardForm: any
-    // Blackbaud_OpenStoreDirectDebitForm: any
-    // Blackbaud_OpenUpdateCardForm: any
-    // Blackbaud_OpenUpdateDirectDebitForm: any
-    // bbCheckout2_0: any
   }
 }
 
@@ -40,32 +28,37 @@ export const OGRegistrationFormRenderer = ({
 }: Props) => {
   const interval = useRef(setInterval(() => {}))
 
-  const [isReady1, setIsReady1] = useState(false)
-  const [isReady2, setIsReady2] = useState(false)
+  const [isReady, setIsReady] = useState(false)
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const script = document.createElement('script')
-    // script.async = true
+    script.async = true
     script.src = 'https://sky.blackbaudcdn.net/static/reg-form-loader/5/main.js'
-    document.head.appendChild(script)
 
-    window.BBDonorFormLoader
+    document.head.appendChild(script)
+    interval.current = setInterval(() => {
+      if (!isReady) {
+        if (window.BBEventRegistrationFormLoader) {
+          setIsReady(true)
+          window.BBEventRegistrationFormLoader.newEventRegistrationForm(window)
+          clearInterval(interval.current)
+        }
+      } else {
+        clearInterval(interval.current)
+      }
+    }, 200)
 
     return () => {
+      clearInterval(interval.current)
+
       // Clean up scripts
-      // document.head.removeChild(script)
-      // const UrlsForRemoval = ['payments.blackbaud.com']
-      // const headScriptsArray = Array.from(
-      //   document.head.getElementsByTagName('script')
-      // )
-      // headScriptsArray.forEach(scriptElement => {
-      //   if (UrlsForRemoval.some(url => scriptElement.src.includes(url))) {
-      //     document.head.removeChild(scriptElement)
-      //   }
-      // })
+      document.head.removeChild(script)
+
       // Clean up variables
+      window.BBEventRegistrationFormLoader = undefined
+      window.iFrameResize = undefined
     }
-  }, [formId])
+  }, [])
 
   const styles = {
     form: css`
@@ -76,15 +69,9 @@ export const OGRegistrationFormRenderer = ({
   return (
     <div
       css={styles.form}
-      // data-blackbaud-registration-form
-      // data-blackbaud-registration-form-envid={envId}
-      // data-blackbaud-registration-form-id={formId}
-      // data-blackbaud-registration-form-zone="usa"
-      // data-blackbaud-registration-form-header-height="0"
-
       data-blackbaud-registration-form
-      data-blackbaud-registration-form-envid="p-iuV2J0KtmEu0x4rOhoISFg"
-      data-blackbaud-registration-form-id="68e95cfa-a53a-4e70-b35b-fb9162213f73"
+      data-blackbaud-registration-form-envid={envId}
+      data-blackbaud-registration-form-id={formId}
       data-blackbaud-registration-form-zone="usa"
       data-blackbaud-registration-form-header-height="0"
       {...props}
